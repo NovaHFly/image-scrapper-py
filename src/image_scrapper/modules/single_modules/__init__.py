2d8c1ba0de1b75b212ea3d9762234311f7dbe526
NovaHFly <mugiviska@gmail.com>
2023-07-28 11:06:26 +0300

    Added is_module function to replace long logic expression in module names gatherer

--- src/image_scrapper/modules/single_modules/__init__.py
+++ src/image_scrapper/modules/single_modules/__init__.py
@@ -4,13 +4,31 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable
 
-from icecream import ic
-
 from image_scrapper.api import PageParser, get_api
 from image_scrapper.helpers import read_cookies
 
 
 SELF_PATH = Path(__file__).absolute().parent
+
+def is_module(path: Path) -> bool:
+    """Checks if path provided corresponds to supported module.
+    
+    Application supports single python files and python packages."""
+    
+    # Valid module name does not start with undescore
+    if path.name.startswith('_'):
+        return False
+    
+    # Single python file module
+    if path.suffix == '.py' and not path.stem == '__init__':
+        return True
+    
+    # Python package module
+    if path.is_dir() and path.glob('__init__.py'):
+        return True
+    
+    return False
+    
 
 @dataclass
 class ModuleData:
@@ -50,7 +68,7 @@ def get_module_data(module_name: str) -> ModuleData:
 
 module_names = (
     module_path.stem for module_path in SELF_PATH.glob('*')
-        if module_path.suffix == '.py' and not module_path.stem == '__init__'
+        if is_module(module_path)
 )
 
 module_apis = {
